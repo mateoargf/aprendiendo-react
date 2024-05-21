c8d706fa36fdd3b1e4f776017ae8b36cb2959601
Mateo Gómez Fleytas <mateogf@hotmail.com>
2024-05-21 01:59:17 -0300

    arrancando video2 minutos 16

---
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react"
+
+const TURNS = {
+  X: 'x',
+  O: 'o'
+}
+
+
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? 'is-selected' : ''}`
+  return (
+    <div className={className}>
+      {children}
+    </div>
+  )
+}
+
+function App() {
+  const [board, setBoard] = useState(Array(9).fill(null))
+
+  const [turn, setTurn] = useState(TURNS.X)
+  return (
+    <main className="board">
+      <h1>tic tac toe</h1>
+      <section className="game">
+        {
+          board.map((_, index) => {
+            return (
+              <Square
+                key={index}
+                index={index}
+              >
+                {board[index]}
+              </Square>
+            )
+          })
+        }
+      </section>
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.X}>{TURNS.O}</Square>
+      </section>
+    </main>
+  )
+}
+
+export default App
